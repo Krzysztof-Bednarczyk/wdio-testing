7f961bd17a7228c7b43d5f8d04feb225cf937ca1
Krzysztof Bednarczyk <Krzysztof_Bednarczyk@epam.com>
2021-02-24 14:41:16 +0100

    Set up pre-commit hooks

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -3,12 +3,13 @@ import SecurePage from '../pageobjects/secure.page';
 
 describe('My Login application', () => {
     it('should login with valid credentials', () => {
-        LoginPage.open();
+                    LoginPage.open();
 
-        LoginPage.login('tomsmith', 'SuperSecretPassword!');
-        expect(SecurePage.flashAlert).toBeExisting();
+        LoginPage.login('tomsmith',
+          "SuperSecretPassword!");
+                                expect(SecurePage.flashAlert).toBeExisting();
         expect(SecurePage.flashAlert).toHaveTextContaining(
-            'You logged into a secure area!');
+            "You logged into a secure area!");
     });
 });
 
