674ed52be4a5d6fd3ad1106448b3ccfbc7061d39
BadNews87 <krisu876@wp.pl>
2021-03-12 17:56:03 +0100

    Feature/feedback form (#7)
    * Fixed linting errors
    
    * Added Feedback Form tests
    
    * Added test job to circleci config
    
    * Updated config.yml
    
    * Revert "Updated config.yml"
    
    This reverts commit 7eb89dde793464b95e48608be8101245d0d2fb63.
    
    * Revert "Added test job to circleci config"
    
    This reverts commit 0edcef210b8724632178f4fdcc1011c581ddb5cc.

--- config/wdio.local.conf.js
+++ config/wdio.local.conf.js
@@ -7,19 +7,23 @@ exports.config = {
     ...{
         capabilities: [{
 
-            // maxInstances can get overwritten per capability. So if you have an in-house Selenium
-            // grid with only 5 firefox instances available you can make sure that not more than
-            // 5 instances get started at a time.
+            /*
+             * maxInstances can get overwritten per capability. So if you have an in-house Selenium
+             * grid with only 5 firefox instances available you can make sure that not more than
+             * 5 instances get started at a time.
+             */
             maxInstances: 5,
             //
             browserName: 'chrome',
-            acceptInsecureCerts: true
-            // If outputDir is provided WebdriverIO can capture driver session logs
-            // it is possible to configure which logTypes to include/exclude.
-            // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
-            // excludeDriverLogs: ['bugreport', 'server'],
+            acceptInsecureCerts: true,
+            /*
+             * If outputDir is provided WebdriverIO can capture driver session logs
+             * it is possible to configure which logTypes to include/exclude.
+             * excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
+             * excludeDriverLogs: ['bugreport', 'server'],
+             */
         }],
         services: ['selenium-standalone'],
-    }
+    },
 
-}
+};
---
+++ test/specs/feedbackForm.spec.js
@@ -0,0 +1,26 @@
+describe('When visiting Zero Bank Web App', () => {
+
+    beforeEach(() => {
+        browser.url('http://zero.webappsecurity.com/index.html');
+        $('#feedback').waitForExist();
+        $('#feedback').click();
+    });
+
+    it('You are able to navigate to feedback form', () => {
+        const feedbackForm = $('form .form-inputs');
+        expect(feedbackForm).toBeVisible();
+    });
+
+    it('You are able to send feedback', () => {
+        $('form .form-inputs').waitForExist();
+        $('#name').setValue('Krzysztof');
+        $('#email').setValue('Krzysztof@wp.pl');
+        $('#subject').setValue('My message to Zero Bank');
+        $('#comment').setValue('Where\'s my monney. You got my money');
+        $('input[value="Send Message"]').click();
+        const formMessage = $('.offset3.span6').getText().split('\n')[1];
+        expect(browser).toHaveUrl('http://zero.webappsecurity.com/sendFeedback.html');
+        expect(formMessage).toEqual('Thank you for your comments, Krzysztof. They will be reviewed by our Customer Service staff and given the full attention that they deserve.');
+    });
+
+});
